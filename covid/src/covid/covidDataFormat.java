f43ee9c4771728a66cbbab0c34011e51aa0e28e2
윤영진 <alwns28@kookmin.ac.kr>
2022-01-09 17:55:55 +0900

    [code] data input function add.
    * 엑셀에 데이터를 입력하는 함수 추가
    * 데이터 포멧 정의하는 클래스 추가

---
+++ covid/src/covid/covidDataFormat.java
@@ -0,0 +1,37 @@
+package covid;
+
+public class covidDataFormat {
+	
+	private String name;
+	private int age;
+	private String area;
+	private String vaccine;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getVaccine() {
+		return vaccine;
+	}
+	public void setVaccine(String vaccine) {
+		this.vaccine = vaccine;
+	}
+	
+	
+
+}
--- covid/src/covid/covidDatabaseHandling.java
+++ covid/src/covid/covidDatabaseHandling.java
@@ -25,10 +25,11 @@ public class covidDatabaseHandling {
 	
 	private String filePath;
 	File file;
+	covidUtils util = new covidUtils();
 	
 	public covidDatabaseHandling(String fileName){
-		filePath = "/Users/yoon-yeoungjin/Desktop/git_repository/covid_management_program/data/" + fileName + ".xls";
-		file = new File(filePath);
+		this.filePath = "/Users/yoon-yeoungjin/Desktop/git_repository/covid_management_program/data/" + fileName + ".xls";
+		this.file = new File(filePath);
 		
 		boolean isExists = file.exists();
 		try {
@@ -64,6 +65,41 @@ public class covidDatabaseHandling {
 		}
 	}
 	
-	
+	public void inputData(int row, covidDataFormat data) {
+		boolean isExists = file.exists();
+		try {
+			if(!isExists) {
+				
+				util.printError("Not exists file !!");
+			}
+			else {
+				file.createNewFile();
+				FileOutputStream fos = new FileOutputStream(file);
+				
+				// 엑셀파일 객체 생성 
+				HSSFWorkbook workbook = new HSSFWorkbook();
+				// 엑셀파일의 시트 생성 
+				HSSFSheet sheet = workbook.createSheet("user");
+				
+				// 시트의 행 생성 
+				Row headerRow = sheet.createRow(row);
+				
+				// 시트의 열 생성
+				Cell cell1 = headerRow.createCell(0);
+				cell1.setCellValue(data.getName());
+				Cell cell2 = headerRow.createCell(1);
+				cell2.setCellValue(data.getAge());
+				Cell cell3 = headerRow.createCell(2);
+				cell3.setCellValue(data.getArea());
+				Cell cell4 = headerRow.createCell(3);
+				cell4.setCellValue(data.getVaccine());
+				
+				workbook.write(fos);
+				fos.close();
+			}
+		} catch(IOException ex) {
+			ex.getMessage();
+		}
+	}
 
 }
